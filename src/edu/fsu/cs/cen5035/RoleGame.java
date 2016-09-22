0144ec234db45be1c0f11908051d3812ea6d567d
andrewsosa001 <andrewsosa001@gmail.com>
2016-09-22 08:05:03 -0400

    Integrate new weapons
    Adds Axe, Arrow, and MagicStaff to the WeaponFactory.
    Adds damage display to RoleGame driver.

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -18,6 +18,20 @@ public class RoleGame {
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
         //TODO: Add the remainig weapons here
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        System.out.println("Arrows are able to do " + arrow.hit(0) + " of damage due to an armor with " +0+ " points.");
+        System.out.println("Arrows are able to do " + arrow.hit(armor) + " of damage due to an armor with " +armor+ " points.");
+
+        Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        System.out.println("Axes are able to do " + axe.hit(0) + " of damage due to an armor with " +0+ " points.");
+        System.out.println("Axes are able to do " + axe.hit(armor) + " of damage due to an armor with " +armor+ " points.");
+
+        Weapon magicstaff = WeaponFactory.getWeapon("magicstaff");
+        System.out.println("Magic Staff has " + magicstaff.hit() + " of damage.");
+        System.out.println("Staffs are able to do " + magicstaff.hit(0) + " of damage due to an armor with " +0+ " points.");
+        System.out.println("Staffs are able to do " + magicstaff.hit(armor) + " of damage due to an armor with " +armor+ " points.");
 
     }
 }
